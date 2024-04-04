5da909c4e3cdccc0a338792f69dd61074dcce810
ChandulaJ <chandugoods3000@gmail.com>
2024-04-04 19:13:16 +0530

    Initial commit

---
+++ src/main/java/org/example/AgeChecker.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class AgeChecker {
+
+    public static boolean isAgeValid(int age) {
+
+        return age>=18 && age<=65;
+    }
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        AgeChecker checkAgeClass = new AgeChecker();
+//        boolean result = AgeChecker.isAgeValid(20);
+//        System.out.println(result);
+    }
+}
